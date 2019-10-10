477a4df9981edfc2e0ece32f78766ab0da03345d
Léo Nantermoz <l.nantermoz@laposte.net>
2019-10-10 10:12:07 +0200

    [UPDATE] UIAnimTest

--- Assets/Scripts/UIAnimations/UIAnimTest.cs
+++ Assets/Scripts/UIAnimations/UIAnimTest.cs
@@ -21,19 +21,28 @@ public class UIAnimTest : MonoBehaviour
 	public void RescaleUpDownAnim(Transform transform) { 
 		Sequence scalingUpDown = DOTween.Sequence();
 		scalingUpDown.Append(transform.DOScale(new Vector3(1.2f, 1.2f, 1), 0.2f));
-		scalingUpDown.Append(transform.DOScale(new Vector3(1f, 1f, 1f), 0.1f));
+		scalingUpDown.Append(transform.DOScale(new Vector3(0.9f, 0.9f, 0.9f), 0.1f));
+		scalingUpDown.Append(transform.DOScale(new Vector3(1.1f, 1.1f, 1.1f), 0.15f));
+		scalingUpDown.Append(transform.DOScale(new Vector3(1f, 1f, 1f), 0.2f));
 	}
 
-	public void ColorChangeForWinnerAnim(Text text) {
-		text.color = Color.cyan;
+	public void ColorChangeForWinnerAnim(Text text, Color firstColor, Color secondColor) {
+		text.color = secondColor;
 		Sequence colorChange = DOTween.Sequence();
-		colorChange.Append(text.DOColor(Color.yellow, 0.5f));
-		colorChange.Append(text.DOColor(Color.cyan, 0.5f));
+		colorChange.Append(text.DOColor(firstColor, 1.5f));
+		colorChange.Append(text.DOColor(secondColor, 1.5f));
 		colorChange.SetLoops(-1, LoopType.Yoyo);
 	}
 
 	public void ColorChangeForLoserAnim(Text text) {
 		Color startingColor = text.color;
-		text.DOColor(new Color(startingColor.r, startingColor.g, startingColor.b, 0.5f),0.5f);
+		text.DOColor(new Color(startingColor.r, startingColor.g, startingColor.b, 0.5f),1f);
 	}
+
+	/*public void ArriveFromLeftAnim(Transform transform) {
+		Sequence arrivingLeft = DOTween.Sequence();
+		arrivingLeft.Append(transform.DOMoveX(190, 1f));
+		arrivingLeft.Append(transform.DOMoveX(10, 0.2f));
+		arrivingLeft.Append(transform.DOMoveX(190, 0.2f));
+	}*/
 }
