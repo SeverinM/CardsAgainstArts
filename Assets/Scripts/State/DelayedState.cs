8b699628527b88dde563beb0838a154a07bb3c19
SeverinM <severinmichaut@gmail.com>
2019-10-11 10:29:33 +0200

    Ecran de debut

---
+++ Assets/Scripts/State/DelayedState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class DelayedState : AbstractRoomState
+{
+    [SerializeField]
+    float secs;
+
+    public override void Init()
+    {
+        base.Init();
+        StartCoroutine(NextState());
+    }
+
+    IEnumerator NextState()
+    {
+        yield return new WaitForSeconds(secs);
+        Manager.GetInstance().stateHolder.NextState();
+    }
+}
